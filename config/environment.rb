96b6f4b74a5abe2fb317247f307ab3478dcb4434
Michael McCullough <michael.mccullough21@gmail.com>
2019-11-01 14:27:35 +0000

    db created

--- config/environment.rb
+++ config/environment.rb
@@ -3,10 +3,7 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
-
-require './app/controllers/application_controller'
+configure :development do
+  set :database, 'sqlite3:db/databse.db'
+end
 require_all 'app'

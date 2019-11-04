6951bca6512139a3ab3766791309c7635d8f4d09
Michael McCullough <michael.mccullough21@gmail.com>
2019-11-04 14:55:18 +0000

    flash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,25 @@
 require './config/environment'
+require 'rack-flash'
 
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
+    use Rack::Flash, sweep: true
   end
 
   get "/" do
-    erb :welcome
+    erb :"users/index"
   end
+
+  #welcome page
+  #signin/login
+  #task show page have edit / "complete(delete)"
+  #profile with add or delete(check mark) task or edit a task
+  #logout
+
 
 end

6cb7a7f81e9750bc36ddd4a98747c2c2445ad046
Michael McCullough <michael.mccullough21@gmail.com>
2019-11-05 14:20:06 +0000

    helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,16 @@ class ApplicationController < Sinatra::Base
   #task show page have edit / "complete(delete)"
   #profile with add or delete(check mark) task or edit a task
   #logout
+  helpers do
+    def logged_in?
+      !!current_user
+    end
 
+    def current_user
+      @user = User.find(session[:user_id])
+    end
+
+  end
+  end
 
 end

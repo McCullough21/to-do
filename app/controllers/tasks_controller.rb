e32ed1dd4e39c3f1a9301f4ad988839d72c8bb76
Michael McCullough <michael.mccullough21@gmail.com>
2019-11-06 14:33:07 +0000

    task create and show

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,5 +5,23 @@ class TasksController < ApplicationController
    erb :"tasks/create"
  end
 
+ post '/new' do
+   @user = current_user
+   if params[:title].empty?
+     flash[:message] = "* Task must have a title *"
+     redirect '/new'
+   else
+   @task = Task.create(params)
+   @task.user_id = @user.id
+   @task.save
+   redirect "/profile/#{@user.id}"
+   end
+ end
+
+ get '/tasks/:id' do
+   @task = Task.find(params[:id])
+   erb :"tasks/show"
+ end
+
 
 end
